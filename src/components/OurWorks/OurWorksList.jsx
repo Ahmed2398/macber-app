fe256cf266addbd84b3e74306b67cb4c58857790
Ahmed Ashraf <ahmedashraf2398@gmail.com>
2023-04-05 16:57:39 +0200

    works Popup

--- src/components/OurWorks/OurWorksList.jsx
+++ src/components/OurWorks/OurWorksList.jsx
@@ -1,56 +1,97 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Container, Col, Row } from "react-bootstrap";
 import {works} from '../../Data/WorksData'
 import OurWorks from './OurWorks';
 import {motion} from "framer-motion";
 import {IoIosArrowForward} from 'react-icons/io';
+import {MDBCard,MDBCardImage} from "mdb-react-ui-kit";
 const OurWorksList = () => {
-  return (
-    <>
-        <motion.section
-            whileInView={{ opacity: [0, 1] }}
-            transition={{ duration: 1, type: 'tween' }}
-            className="work ptb-60"
-        >
-            <Container>
-        <Row>
-          <Col lg={12}>
-            <div className="head-content pb-3">
-              <h4 className="small-text text-warning text-center mb-5">
-                Our Works
-              </h4>
-            </div>
-          </Col>
-        </Row>
 
-        <Row>
-            {works.map((work, index)=>{
-                const {id, title, description, img} = work;
-                return(
-                    <Col lg={2} md={6} sm={12} className="pb-3" key={index}>
-                          <div className="box-work h-100  ">
-                            <OurWorks key={id} title={title} description={description} img={img}/>
-                          </div>
-                    </Col>
-                )
-            })}
-          
-         
-         </Row>
- 
-         <div lg={12} className="pt-3 text-end">
-           <a
-             href={`/softwaredev`}
-             className=""
-           >
-           <IoIosArrowForward className="arrow-link"/>
-           </a>
-         </div>
-       </Container>
-        </motion.section>
-     
-    </>
-  )
+    const [popupcontent, setpopupcontent] = useState([]);
+    const [popuptogle, setpopuptogle] = useState(false);
+    const changecontent=(project)=>{
+        setpopupcontent([project]);
+        setpopuptogle(!popuptogle);
+    }
+    return (
+        <>
+            <motion.section
+                whileInView={{ opacity: [0, 1] }}
+                transition={{ duration: 1, type: 'tween' }}
+                className="work ptb-60"
+            >
+                <Container>
+                    <Row>
+                        <Col lg={12}>
+                            <div className="head-content pb-3">
+                                <h4 className="small-text text-warning text-center mb-5">
+                                    Our Works
+                                </h4>
+                            </div>
+                        </Col>
+                    </Row>
+                <Row>
+
+                    <div className="app_container">
+                        <div className="content_container">
+                            {works.map((work,index)=>{
+                                return(
+                                    <MDBCard className="h-100 box-work my-3">
+                                        <button className="btn "  onClick={()=> changecontent(work)}>
+                                        <MDBCardImage src={work.imgCard} fluid alt={work.title} className="rounded" />
+                                        </button>
+                                    </MDBCard>
+                                );
+                            })}
+                        </div>
+
+                        {popuptogle && (
+                            <div className="pop_up_container" onClick={changecontent}>
+                                <div className="pop_up_body scroll" onClick={(e)=> e.stopPropagation()}>
+                                    <div className="pop_up_header">
+                                        <button className="btn btn-purble m-2" onClick={changecontent}>x</button>
+                                    </div>
+                                    <div className="pop_up_content">
+                                        {popupcontent.map((pop)=>{
+                                            return(
+                                                <div className="pop_up_card">
+                                                    {/*<h3>{pop.title}</h3>*/}
+                                                    {/*<p>{pop.description}</p>*/}
+                                                    {/*<p>{pop.id}</p>*/}
+                                                    {/*<p>{pop.details}</p>*/}
+                                                    <img className="pdf" src={pop.details} alt={pop.title}/>
+                                                </div>
+                                            )
+                                        })}
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        )}
+                    </div>
+                    <div lg={12} className="pt-3 text-end">
+                        <a
+                            href={`/softwaredev`}
+                            className=""
+                        >
+                            <IoIosArrowForward className="arrow-link"/>
+                        </a>
+                    </div>
+                </Row>
+
+
+
+
+
+
+
+
+            </Container>
+            </motion.section>
+
+        </>
+    )
 }
 
 export default OurWorksList
